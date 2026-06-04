0ebf82e319694d0e72d253b0a8a6e3d528c3df3d
sabaka-chabaka <gamekimg360@gmail.com>
2026-06-04 15:02:58 +0400

    Identity domain

---
+++ SkeletonBackend.Domain/Identity/RefreshToken.cs
@@ -0,0 +1,14 @@
+namespace SkeletonBackend.Domain.Identity;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string Token { get; set; } = null!;
+
+    public DateTime ExpiresAt { get; set; }
+
+    public bool Revoked { get; set; }
+}
---
+++ SkeletonBackend.Domain/Identity/User.cs
@@ -0,0 +1,11 @@
+namespace SkeletonBackend.Domain.Identity;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+    public DateTime? LastLoginAt { get; set; }
+}
